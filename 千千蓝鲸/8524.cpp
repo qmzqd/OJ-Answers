bad10e60e9fb11ec9e1b4b61ead85899a98f9b04
qmzqd <3233425135@qq.com>
2024-07-21 14:12:31 +0800

    添加代码

--- 千千蓝鲸/8524.cpp
+++ 千千蓝鲸/8524.cpp
@@ -1,7 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MOD = 100003; 
+int n, m;
+int a[1005][1005];    
+bool flag[1005][1005]; 
 int main()
 {
-   
-   return 0;
+    cin >> n >> m; 
+    while (m--)
+    {
+        int x, y;
+        cin >> x >> y;
+        flag[x][y] = 1; 
+    }
+    a[1][1] = 1;                 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++) 
+        {
+            a[i][j] += a[i - 1][j] + a[i][j - 1]; 
+            if (flag[i][j] == 1)
+                a[i][j] = 0;         
+            a[i][j] = a[i][j] % MOD; 
+        }
+    }
+    cout << a[n][n];
+    return 0;
 }
